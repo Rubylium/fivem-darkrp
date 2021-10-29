0b07238f521793162cf9617f3ef1ed40972a8709
Alexis OKOROKOFF <alexisoko0@gmail.com>
2021-10-29 11:55:14 +0200

    style: Add main structure

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+client_scripts {
+    "client/config.lua"
+}
+
+shared_scripts {
+    "shared/config.lua"
+}
+
+server_scripts {
+    "server/config.lua"
+}
